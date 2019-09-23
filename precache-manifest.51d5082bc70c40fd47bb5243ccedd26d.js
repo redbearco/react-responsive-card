e970e23af36432c36accbd7cbd5d1762f6aff722
redbear <rbear.co@gmail.com>
2019-09-23 11:11:04 +0330

    Updates

---
+++ precache-manifest.51d5082bc70c40fd47bb5243ccedd26d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375912acb841a0dae28ccf5dc2ea4788",
+    "url": "/react-responsive-card/index.html"
+  },
+  {
+    "revision": "1d5775dbdd8733894ccd",
+    "url": "/react-responsive-card/static/css/main.928d28e8.chunk.css"
+  },
+  {
+    "revision": "223dfdc619306ab14443",
+    "url": "/react-responsive-card/static/js/2.55679640.chunk.js"
+  },
+  {
+    "revision": "1d5775dbdd8733894ccd",
+    "url": "/react-responsive-card/static/js/main.e073adc0.chunk.js"
+  },
+  {
+    "revision": "8342bc4279eb8a365f2b",
+    "url": "/react-responsive-card/static/js/runtime-main.fbcbb34c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-responsive-card/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-responsive-card/precache-manifest.51d5082bc70c40fd47bb5243ccedd26d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-responsive-card/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
